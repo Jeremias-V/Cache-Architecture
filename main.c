e513bcb7cc63b77c09c831b04968178e7f503b55
Jeremias-V <jeremias.villalobos.t@gmail.com>
2021-05-07 18:54:46 -0500

    Typedef

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
 
+#define BLOCK_SIZE 8
+#define NUMBER_OF_BLOCKS 4
+
+typedef struct{
+
+    unsigned int V : 1;
+    unsigned int Tag : 2;
+    unsigned int Data : 3;
+
+} Set;
+
+
 int main(){
-    printf("Compiles!\n");    
+
+    Set test[NUMBER_OF_BLOCKS][BLOCK_SIZE];
+    //printf("Size of SET: %ld\n", sizeof(test[0][0]));
+    for(int i = 0; i < 9; i++){
+        printf("V: %d\t\tTag: %d\t\tData: %d\n", test[0][0].V++, test[0][0].Tag++, test[0][0].Data++);
+    }
     return 0;
 }
